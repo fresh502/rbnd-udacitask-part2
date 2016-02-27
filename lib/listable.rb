a1fa8ab8e326cc6f9462b8a09aff3aa3c49dfca9
fresh502 <fresh502@gmail.com>
2016-02-27 16:57:49 +0900

    Move format_date method to Listable module

--- lib/listable.rb
+++ lib/listable.rb
@@ -2,4 +2,14 @@ module Listable
 	def format_description(description)
     "#{description}".ljust(25)
   end
+  def format_date(options={})
+		if options.has_key?(:start_date) || options.has_key?(:end_date)
+			dates = options[:start_date].strftime("%D") if options[:start_date]
+			dates << " -- " + options[:end_date].strftime("%D") if options[:end_date]
+			dates = "N/A" if !dates
+			return dates
+		elsif options.has_key?(:due)
+			options[:due] ? options[:due].strftime("%D") : "No due date"
+		end
+	end
 end
